d9a8233e563738296fe5a9cf3fd5be0980b61204
Viviam Solangeli Bermudez Paiva <viviambermudez@gmail.com>
2026-03-11 14:11:43 +0100

    Delete celios from Modules

--- Modules/celios/src/celios/utils/__init__.py
+++
@@ -1,9 +0,0 @@
-from .report import activitymatrix_report, cellfiles_report
-from .io import save_file, load_csv_file
-
-__all__ = [
-	"activitymatrix_report",
-	"cellfiles_report",
-	"save_file",
-	"load_csv_file",
-]
